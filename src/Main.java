7d4c77b62c748c1b0c4904fcf9984f30366ae6ee
000ubird <000ubird@gmail.com>
2014-11-04 00:47:54 +0900

    Mainメソッドを変更
    StructClassオブジェクトとStructFieldオブジェクトを生成し、それぞれの情報を表示するように変更

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
 
 public class Main {
 	private static String CLASSPATH = "./bin/ans/";		//クラスファイルのディレクトリ
-	private static String CLASSNAME = "Point";			//読み込む.classファイル名
+	private static String CLASSNAME = "LineSegment";			//読み込む.classファイル名
 	
 	public static void main(String[] args) throws Exception{
 		//クラスファイル読み込み
@@ -13,28 +11,14 @@ public class Main {
 	    System.out.println(c);
 	    
 	    //クラス名取得
-	    System.out.println(c.getName());
-	    System.out.println(Modifier.isInterface(c.getModifiers()));
-	    System.out.println(c.getSuperclass());
+		StructClass sc = new StructClass(c);
+		System.out.println(sc);
 	    
 	    //フィールド取得
 	    Field[] fields = c.getDeclaredFields();
 	    for(Field field : fields){
-	    	System.out.println(field.getModifiers()+ " => "+ field.getName() +" : "+ field.getType());
-	    }
-	    
-	    //メソッド取得
-	    Method[] methods = c.getMethods();
-	    for(Method method : methods){
-	    	String name = method.getName();
-	    	String returnType = method.getReturnType().getName();
-			Class<?>[] paramType = method.getParameterTypes();
-	    	
-	    	System.out.print("Name => "+name+" , return => "+returnType);
-	    	for(Class<?> type : paramType){
-	    		System.out.print(" , paramType => "+type);
-	    	}
-	    	System.out.println();
+ 	    	StructField sf = new StructField(field.getName(),field.getType(),field.getModifiers());
+	    	System.out.println(sf);
 	    }
 	}
 }
