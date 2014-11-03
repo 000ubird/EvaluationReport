b190e634edbb0a08b75aace259928fadbeb293cb
000ubird <000ubird@gmail.com>
2014-11-04 00:10:04 +0900

    StructFieldクラス-引数有りのコンストラクタを新たに定義。
    また、フィールド変数の修飾子の種類を文字列で返すメソッドを定義

--- src/StructField.java
+++ src/StructField.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Modifier;
+
 /**
  * フィールド変数の情報を保持するクラスです。
  * 変数名、型、修飾子をフィールドに持ちます。
@@ -6,9 +8,32 @@
  */
 public class StructField {
 	private String name;		//フィールド変数名
-	private Class fieldType;	//フィールド変数の型
+	private Class<?> fieldType;	//フィールド変数の型
 	private String modifier;	//フィールド変数の修飾子
 	
 	public StructField(){
 	}
+	
+	public StructField(String name,Class type,int mod){
+		this.name = name;
+		this.fieldType = type;
+		this.modifier = getModifierType(mod);
+	}
+	
+	/**
+	 * フィールド変数の修飾子の種類を文字列で返す
+	 * @param mod 修飾子の種類を表すint型の数値
+	 * @return 修飾子を表す文字列
+	 */
+	public static String getModifierType(int mod){
+		String buf = "none";
+		
+		if(Modifier.isPublic(mod)) buf = "public";
+		if(Modifier.isPrivate(mod)) buf = "private";
+		if(Modifier.isProtected(mod)) buf = "protected";
+		if(Modifier.isFinal(mod)) buf = "final";
+		
+		return buf;
+	}
+
 }
