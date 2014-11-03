2f410abe37c56c3e583f452b2f77c65dc31fe1f6
000ubird <000ubird@gmail.com>
2014-11-03 23:08:10 +0900

    クラス構造クラスにて、クラスを引数に取って生成するコンストラクタを追加。
    他、修飾子を文字列化するメソッドを追加。返り値の文字列が正しくない場合あり。

--- src/StructClass.java
+++ src/StructClass.java
@@ -1,3 +1,5 @@
+import java.lang.reflect.Modifier;
+
 /**
  * クラス構造を保持するクラスです。
  * クラス名、修飾子、継承の有無、インターフェースをフィールドに持ちます。
@@ -12,4 +14,31 @@ public class StructClass {
 	
 	public StructClass(){
 	}
+	
+	/**
+	 * クラスを引数に取ってコンストラクタを生成
+	 * @param c	クラス
+	 */
+	public StructClass(Class<?> c) {
+		name = c.getName();
+		modifiers = getModifierType(c.getModifiers());
+		inheritance = c.getSuperclass();
+		interfaces = c.getInterfaces();
+	}
+	
+	/**
+	 * クラスの修飾子の種類を文字列で返す
+	 * @param mods 修飾子の種類を表すint型の数値
+	 * @return 修飾子を表す文字列
+	 */
+	public static String getModifierType(int mods){
+		String buf = "none";
+		
+		if(Modifier.isPublic(mods)) buf = "public";
+		if(Modifier.isAbstract(mods)) buf = "abstruct";
+		if(Modifier.isFinal(mods)) buf = "final";
+		
+		return buf;
+	}
+	
 }
