3c4b2f30413981f06b9c98c027261e2c21c827c1
000ubird <000ubird@gmail.com>
2014-11-03 23:48:49 +0900

    フィールド変数の情報を保持するクラスを定義
    変数名、型、修飾子情報をフィールドに持つ。また、デフォルトコンストラクタを定義。

---
+++ src/StructField.java
@@ -0,0 +1,14 @@
+/**
+ * フィールド変数の情報を保持するクラスです。
+ * 変数名、型、修飾子をフィールドに持ちます。
+ * @author bp12084
+ *
+ */
+public class StructField {
+	private String name;		//フィールド変数名
+	private Class fieldType;	//フィールド変数の型
+	private String modifier;	//フィールド変数の修飾子
+	
+	public StructField(){
+	}
+}
