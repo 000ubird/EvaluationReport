585efb1ec6dd7833b858923bf99a40038506d10e
000ubird <000ubird@gmail.com>
2014-11-06 00:04:04 +0900

    MainメソッドにStructMethodオブジェクトの生成テストを記述
    この処理はメソッド化しクラス構造クラスに追加予定。

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 public class Main {
 	private static String CLASSPATH = "./bin/ans/";		//クラスファイルのディレクトリ
@@ -14,7 +15,18 @@ public class Main {
 		StructClass sc = new StructClass(c);
 		System.out.println(sc);
 	    
-	    //フィールド取得
+		//メソッド取得 この処理はClassStructureクラスに記述予定
+		Method[] methods = c.getMethods();
+		for(Method method : methods){
+			String name = method.getName();							//メソッド名取得
+			String returnType = method.getReturnType().getName();	//返り値の型を取得
+			Class<?>[]  paramTypes = method.getParameterTypes();	//引数の型を取得
+			
+			StructMethod sm = new StructMethod(name,returnType,paramTypes);
+			System.out.println(sm);
+		}
+		
+	    //フィールド取得 この処理はClassStructureクラスに記述予定
 	    Field[] fields = c.getDeclaredFields();
 	    for(Field field : fields){
  	    	StructField sf = new StructField(field.getName(),field.getType(),field.getModifiers());
