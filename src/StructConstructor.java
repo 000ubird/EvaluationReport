b96347a7ef6ec9d4e53f0e195fee5a3efe090ad6
000ubird <000ubird@gmail.com>
2014-11-06 01:26:42 +0900

    コンストラクタの情報を保持するStructConstructorクラスを定義
    フィールドにはコンストラクタの引数の型を定義

---
+++ src/StructConstructor.java
@@ -0,0 +1,13 @@
+
+/**
+ * コンストラクタの情報を保持するクラスです。
+ * コンストラクタの引数の型をフィールドに持ちます。
+ * @author bp12084
+ *
+ */
+public class StructConstructor {
+	private String[] paramTypes;	//コンストラクタの引数の型
+	
+	public StructConstructor(){
+	}
+}
