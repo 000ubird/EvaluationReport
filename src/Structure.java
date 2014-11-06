2b4404b3d9080c41bfe35e48999b6953c0b2517d
000ubird <000ubird@gmail.com>
2014-11-06 17:41:43 +0900

    クラス構造情報を保持するクラスStructureクラスを定義
    フィールドにはクラス情報・フィールド情報・コンストラクタ情報・メソッド情報を定義。

---
+++ src/Structure.java
@@ -0,0 +1,14 @@
+/**
+ * クラス構造情報を保持するクラスです。
+ * @author bp12084
+ *
+ */
+public class Structure {
+	private StructClass sClass;			//クラス情報
+	private StructField[] sField;		//フィールド情報
+	private StructConstructor[] sConst;	//コンストラクタ情報
+	private StructMethod[] sMethod;		//メソッド情報
+	
+	public Structure() {
+	}
+}
