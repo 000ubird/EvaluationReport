ff1abcdd50edffa6a4ef49383e426ac6d8f7c7c4
000ubird <000ubird@gmail.com>
2014-11-10 01:14:11 +0900

    StructConstructorクラスにスコア情報の属性と操作を新たに定義
    ・フィールドにスコア情報を定義
    ・コンストラクタでは追加でデフォルトスコアを代入するように修正
    ・スコア情報付きCSV文字列を取得するメソッドを定義
    ・スコアを算出するメソッドを定義
    ・equalsメソッドを定義
    ・toStringメソッドを修正

--- src/StructConstructor.java
+++ src/StructConstructor.java
@@ -7,6 +7,8 @@
  */
 public class StructConstructor {
 	private Class<?>[] paramTypes;	//コンストラクタの引数の型
+	private Score score;			//得点情報
+	private static int MAX_SCORE = 1;	//フィールドスコアの最高得点
 	
 	/**
 	 * 「コンストラクタの引数の型」を引数に取るコンストラクタ
@@ -14,6 +16,7 @@ public class StructConstructor {
 	 */
 	public StructConstructor(Class<?>[] paramTypes){
 		this.paramTypes = paramTypes;
+		score = new Score();
 	}
 	
 	/**
@@ -28,11 +31,44 @@ public class StructConstructor {
 		return "Constructor,"+buf;
 	}
 	
+	/**
+	 * スコア情報付きのCSV文字列を取得する
+	 * @return CSV形式の文字列
+	 */
+	public String getScoreCSV(){
+		return getCSV()+","+score.getCSV();
+	}
+	
+	/**
+	 * コンストラクタ情報を引数に取ってスコアを算出する
+	 * @param sc テストするコンストラクタ構造オブジェクト
+	 */
+	public void calcScore(StructConstructor sc){
+		int currentScore = 0;
+		if(this.equals(sc)) currentScore++;
+		
+		 score = new Score(currentScore,MAX_SCORE);
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		StructConstructor sc = (StructConstructor)o;
+		try{
+			for(int i=0;i<this.paramTypes.length;i++){
+				if(this.paramTypes[i].equals(sc.paramTypes[i])==false) return false;
+			}
+		}catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+		
+		return true;
+	}
+	
 	@Override
 	public String toString(){
 		String buf = "";
 		for(Class<?> c : paramTypes) buf += c.getName() + ",";
 		
-		return "Constructor => "+buf;
+		return "Constructor => "+buf+score;
 	}
 }
