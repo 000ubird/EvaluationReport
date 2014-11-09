7c13638139408d9cd11b1de349463c017f449432
000ubird <000ubird@gmail.com>
2014-11-09 23:02:15 +0900

    StructFieldクラスにスコア情報の属性と操作を新たに定義
    ・コンストラクタは新たにデフォルトのスコア情報を代入する様に修正
    ・指定したフィールド名と一致するフィールドであるかを判定するメソッドを定義
    ・スコア算出メソッドを定義
    ・toStringメソッドはスコア情報を追加で出力するように修正
    ・スコア情報付きのCSV文字列を取得するメソッドを定義

--- src/StructField.java
+++ src/StructField.java
@@ -10,7 +10,13 @@ public class StructField {
 	private String name;		//フィールド変数名
 	private String fieldType;	//フィールド変数の型
 	private String modifier;	//フィールド変数の修飾子
+	private Score score;		//得点情報
+	private static int MAX_SCORE = 3;	//フィールドスコアの最高得点
 	
+	public String getName(){
+		return name;
+	}
+
 	/**
 	 * 引数有りのコンストラクタ
 	 * @param name	フィールド変数名
@@ -21,6 +27,7 @@ public class StructField {
 		this.name = name;
 		this.fieldType = type.getSimpleName();
 		this.modifier = getModifierType(mod);
+		score = new Score();
 	}
 	
 	/**
@@ -41,12 +48,44 @@ public class StructField {
 	}
 	
 	/**
+	 * 指定したフィールド名と一致するフィールドであるかを判定する
+	 * @param name 調べたいフィールド名
+	 * @return     一致したらtrue,一致しなかったらfalse
+	 */
+	public boolean hasField(String name){
+		if(this.name.equals(name)) return true;
+		else return false;
+	}
+	
+	/**
+	 * フィールド構造情報を引数に取ってスコアを算出する
+	 * 算出したスコアはフィールドのスコア情報に保持される
+	 * @param sf フィールド情報
+	 */
+	public void calcScore(StructField sf){
+		int currentScore = 0;
+		if(this.name.equals(sf.name)) 			 currentScore++;
+		if(this.fieldType.equals(sf.fieldType))  currentScore++;
+		if(this.modifier.equals(sf.modifier))	 currentScore++;
+		
+		this.score = new Score(currentScore,MAX_SCORE);
+	}
+	
+	/**
 	 * フィールドの情報をCSV形式の文字列にする
 	 * フォーマット(サンプル)-> Field | 修飾子 | 型 | フィールド名
 	 * @return CSV形式の文字列
 	 */
 	public String getCSV(){
 		return "Field,"+modifier+","+fieldType+","+name;
+	}
+	
+	/**
+	 * スコア情報付きのCSV文字列を取得する
+	 * @return CSV形式の文字列
+	 */
+	public String getScoreCSV(){
+		return "Field,"+modifier+","+fieldType+","+name+","+score.getCSV();
 	}
 	
 	@Override
@@ -61,7 +100,7 @@ public class StructField {
 	
 	@Override
 	public String toString(){
-		return "Field  "+modifier+ " => " +name+ " : " +fieldType;
+		return "Field  "+modifier+ " => " +name+ " : " +fieldType+" -> "+score;
 	}
 
 }
